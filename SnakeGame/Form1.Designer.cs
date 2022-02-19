bed7ac6d60a919bf3c4f50f7447f608aa5b9ae1c
Berat <Berat@DESKTOP-CEI400U>
2022-02-19 21:44:45 +0300

    Add project files.

---
+++ SnakeGame/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPuan = new System.Windows.Forms.Label();
+            this.lblSure = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timerYilanHareket = new System.Windows.Forms.Timer(this.components);
+            this.timerSure = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(23, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 400);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(40, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Puan:";
+            // 
+            // lblPuan
+            // 
+            this.lblPuan.AutoSize = true;
+            this.lblPuan.ForeColor = System.Drawing.Color.White;
+            this.lblPuan.Location = new System.Drawing.Point(100, 22);
+            this.lblPuan.Name = "lblPuan";
+            this.lblPuan.Size = new System.Drawing.Size(21, 24);
+            this.lblPuan.TabIndex = 3;
+            this.lblPuan.Text = "0";
+            // 
+            // lblSure
+            // 
+            this.lblSure.AutoSize = true;
+            this.lblSure.ForeColor = System.Drawing.Color.White;
+            this.lblSure.Location = new System.Drawing.Point(583, 22);
+            this.lblSure.Name = "lblSure";
+            this.lblSure.Size = new System.Drawing.Size(21, 24);
+            this.lblSure.TabIndex = 5;
+            this.lblSure.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(523, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Süre:";
+            // 
+            // timerYilanHareket
+            // 
+            this.timerYilanHareket.Tick += new System.EventHandler(this.timerYilanHareket_Tick);
+            // 
+            // timerSure
+            // 
+            this.timerSure.Interval = 1000;
+            this.timerSure.Tick += new System.EventHandler(this.timerSure_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat SemiBold", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(234, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 35);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SNAKE GAME";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(23, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(600, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(641, 502);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSure);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblPuan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Montserrat SemiBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPuan;
+        private System.Windows.Forms.Label lblSure;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timerYilanHareket;
+        private System.Windows.Forms.Timer timerSure;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SnakeGame/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+
+        private Label yilanUcu;
+        private int YilanParcasiArasiMesafe=2;
+        private int yilanParcasiSayisi;
+        private int yilanBoyutu=20;
+        private int yemBoyutu = 20;
+        private Label yem;
+        private Random random;
+        private HareketYonu yon;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Sifirla();
+
+
+
+        }
+
+        private void YenidenBaslat()
+        {
+            lblPuan.Text = "0";
+            lblSure.Text = "0";
+
+            Sifirla();
+
+
+        }
+
+        public void Sifirla()
+        {
+            panel1.Controls.Clear();
+            yilanParcasiSayisi = 0;
+            YemOlustur();
+            yeminYeriniDegistir();
+            yilaniYerlestir();
+            timerYilanHareket.Enabled = true;
+            timerSure.Enabled = true;
+
+        }
+
+        private Label YilanParcasiOlustur(int locationX, int locationY)
+        {
+            yilanParcasiSayisi++;
+            Label lbl = new Label()
+            {
+                Name = "yilanParca" + yilanParcasiSayisi,
+                BackColor = Color.Red,
+                Width = yilanBoyutu,
+                Height = yilanBoyutu,
+                Location = new Point(locationX, locationY)
+
+            };
+            this.panel1.Controls.Add(lbl);
+            return lbl;
+
+        }
+
+        private void yilaniYerlestir()
+        {
+            yilanUcu = YilanParcasiOlustur(0, 0);
+            yilanUcu.Text = ":";
+            yilanUcu.TextAlign = ContentAlignment.MiddleCenter;
+            yilanUcu.ForeColor = Color.White;
+            var locationX = (panel1.Width / 2) - (yilanUcu.Width / 2);
+            var locationY = (panel1.Height / 2) - (yilanUcu.Height / 2);
+            yilanUcu.Location = new Point(locationX, locationY);
+        }
+
+        private void YemOlustur()
+        {
+            Label lbl = new Label()
+            {
+                Name = "yem",
+                BackColor = Color.White,
+                Width = yemBoyutu,
+                Height = yemBoyutu
+                
+
+            };
+            yem = lbl;
+            this.panel1.Controls.Add(lbl);
+            
+        }
+
+        private void yeminYeriniDegistir()
+        {
+            var locationX = 0;
+            var locationY = 0;
+
+            bool durum;
+
+            do
+            {
+                durum = false;
+                locationX = random.Next(0, panel1.Width - yemBoyutu);
+                locationY = random.Next(0, panel1.Height - yemBoyutu);
+                var rect1 = new Rectangle(new Point(locationX, locationY), yem.Size);
+
+
+                foreach (Control control in panel1.Controls)
+                {
+                    if (control is Label && control.Name.Contains("yilanParca"))
+                    {
+                        var rect2 = new Rectangle(control.Location, control.Size);
+
+                        if (rect1.IntersectsWith(rect2))
+                        {
+                            durum = true;
+                            break;
+
+
+                        }
+                    }
+                }
+
+            } while (durum);
+
+            yem.Location = new Point(locationX, locationY);
+
+        }
+
+
+        private enum HareketYonu
+        {
+            Yukari,
+            Asagi,
+            Sola,
+            Saga
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            var keyCode = e.KeyCode;
+
+            if (yon==HareketYonu.Sola && keyCode==Keys.D
+                || yon == HareketYonu.Saga && keyCode == Keys.A
+                || yon == HareketYonu.Yukari && keyCode == Keys.S
+                || yon == HareketYonu.Asagi && keyCode == Keys.W)
+            {
+                return;
+
+            }
+
+            switch (keyCode)
+            {
+                case Keys.W:
+                    yon = HareketYonu.Yukari;
+                    break;
+                case Keys.S:
+                    yon = HareketYonu.Asagi;
+                    break;
+                case Keys.A:
+                    yon = HareketYonu.Sola;
+                    break;
+                case Keys.D:
+                    yon = HareketYonu.Saga;
+                    break;
+                case Keys.P:
+                    timerSure.Enabled = false;
+                    timerYilanHareket.Enabled = false;
+                    break;
+                case Keys.C:
+                    timerSure.Enabled = true;
+                    timerYilanHareket.Enabled = true;
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+
+        private void timerYilanHareket_Tick(object sender, EventArgs e)
+        {
+            YilanUcunuTakipEt();
+            yilaniYurut();
+
+
+            oyunBittimi();
+            yilanYemiYedimi();
+            
+        }
+
+        private void yilaniYurut()
+        {
+            var locationX = yilanUcu.Location.X;
+            var locationY = yilanUcu.Location.Y;
+
+
+            switch (yon)
+            {
+                case HareketYonu.Yukari:
+                    yilanUcu.Location = new Point(locationX, locationY - (yilanUcu.Width + YilanParcasiArasiMesafe));
+                    break;
+                case HareketYonu.Asagi:
+                    yilanUcu.Location = new Point(locationX, locationY + (yilanUcu.Width + YilanParcasiArasiMesafe));
+                    break;
+                case HareketYonu.Sola:
+                    yilanUcu.Location = new Point(locationX - (yilanUcu.Width + YilanParcasiArasiMesafe), locationY);
+                    break;
+                case HareketYonu.Saga:
+                    yilanUcu.Location = new Point(locationX + (yilanUcu.Width + YilanParcasiArasiMesafe), locationY);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        
+        private void oyunBittimi()
+        {
+            bool oyunBittimi = false;
+
+            var rect1 = new Rectangle(yilanUcu.Location, yilanUcu.Size);
+
+            foreach (Control control in panel1.Controls)
+            {
+                if (control is Label && control.Name.Contains("yilanParca")  && control.Name != yilanUcu.Name)
+                {
+                    var rect2 = new Rectangle(control.Location, control.Size);
+                    if (rect1.IntersectsWith(rect2))
+                    {
+                        oyunBittimi = true;
+                        break;
+
+                    }
+                }
+            }
+
+            if (oyunBittimi)
+            {
+                timerYilanHareket.Enabled = false;
+                timerSure.Enabled = false;
+                DialogResult sonuc =MessageBox.Show("Puanınız: "+lblPuan.Text+" | Yeniden Oynamak için Tamama tıkla.", "GAME OVER!",MessageBoxButtons.OKCancel,MessageBoxIcon.Information);
+
+                if (sonuc== DialogResult.OK)
+                {
+                    YenidenBaslat();
+                }
+
+                
+            }
+        }
+
+        private void yilanYemiYedimi()
+        {
+            var rect1 = new Rectangle(yilanUcu.Location, yilanUcu.Size);
+            var rect2 = new Rectangle(yem.Location, yem.Size);
+
+            if (rect1.IntersectsWith(rect2))
+            {
+                lblPuan.Text = (Convert.ToInt32(lblPuan.Text)+10).ToString();
+                yeminYeriniDegistir();
+                YilanParcasiOlustur(-  yilanBoyutu, - yilanBoyutu  );
+
+
+            }
+        }
+
+        private void YilanUcunuTakipEt()
+        {
+            if (yilanParcasiSayisi <= 1) return;
+
+            
+            for (int i = yilanParcasiSayisi; i > 1; i--)
+            {
+                var sonrakiParca = (Label)panel1.Controls[i];
+                var oncekiParca = (Label)panel1.Controls[i-1];
+
+                sonrakiParca.Location = oncekiParca.Location;
+
+            }
+        }
+
+        private void timerSure_Tick(object sender, EventArgs e)
+        {
+            lblSure.Text = (Convert.ToInt32(lblSure.Text)+1).ToString();
+        }
+    }
+}
